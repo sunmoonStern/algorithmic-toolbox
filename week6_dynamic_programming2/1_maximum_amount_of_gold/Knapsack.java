36f2b15c0a11549c0132d3ae52da97a938679140
Mei Sasaki <october.sky.2009@gmail.com>
2019-05-22 22:34:16 +0900

    6-1 passed

--- week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
@@ -4,12 +4,26 @@ public class Knapsack {
     static int optimalWeight(int W, int[] w) {
         //write you code here
         int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
+        int[][] valueStocker = new int[w.length+1][W+1];
+
+        for (int i = 0; i <= w.length; i++) {
+            valueStocker[i][0] = 0;
+        }
+        for (int j = 0; j <= W; j++) {
+            valueStocker[0][j] = 0;
+        }
+        for (int i = 1; i <= w.length; i++) {
+          for (int j = 1; j <= W; j++) {
+              int v = 0;
+              valueStocker[i][j] = valueStocker[i-1][j];
+              // i-th element of w is w[i-1]
+              if (w[i-1] <= j) {
+                  v = valueStocker[i-1][j-w[i-1]] + w[i-1]; // value is proportinal to weight
+              }
+              if (v > valueStocker[i][j]) valueStocker[i][j] = v;
           }
         }
-        return result;
+        return valueStocker[w.length][W];
     }
 
     public static void main(String[] args) {
